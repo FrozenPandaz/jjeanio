49f9120b5ca64b0aed39973b147434ab743b9053
Jason Jean <jasonjean1993@gmail.com>
2018-01-15 05:20:45 -0500

    Turn on initialNavigation and import ModuleMapLoader Module (#16)

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
@@ -8,6 +9,7 @@ import { AppModule } from './app.module';
 	imports: [
 		AppModule,
 		ServerModule,
+		ModuleMapLoaderModule
 	],
 	bootstrap: [AppComponent],
 })
